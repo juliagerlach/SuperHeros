73e0a7a5c343be80d6a6e4dd5a1e1462a0c69b0b
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-02 16:15:30 -0500

    added index action

--- SuperHero/Controllers/SuperHeroController.cs
+++ SuperHero/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperHero.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,25 @@ namespace SuperHero.Controllers
 {
     public class SuperHeroController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: SuperHero
         public ActionResult Index()
         {
-            return View();
+            return View(db.Superhero.ToList());
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Superhero superhero = db.Superhero.Find(id);
+            if (superhero == null)
+            {
+                return HttpNotFound();
+            }
+            return View(superhero);
         }
     }
 }
--- SuperHero/Migrations/Configuration.cs
+++ SuperHero/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace SuperHero.Migrations
 {
+    using SuperHero.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,10 +19,15 @@ namespace SuperHero.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            //var superheroes = new List<Superhero>
-            //{
-            //    new Superhero {Name }
-            //};
+                context.Superhero.AddOrUpdate(
+                  s => s.Name,
+                  new Superhero { Name = "Wonder Woman", Alterego = "Diana Prince", PrimaryAbility = "Superhuman strength", SecondaryAbility = "Empathy", Catchphrase = "Merciful Minerva!"},
+                  new Superhero { Name = "Catwoman", Alterego = "Selena Kyle", PrimaryAbility = "Combat", SecondaryAbility = "Stealth", Catchphrase = "You're catnip to a girl like me." },
+                  new Superhero { Name = "Elektra", Alterego = "Erynys", PrimaryAbility = "Hypnosis", SecondaryAbility = "Silent scream", Catchphrase = "Don't worry. Death's not that bad." },
+                  new Superhero { Name = "Supergirl", Alterego = "Linda Lang", PrimaryAbility = "Flight", SecondaryAbility = "Invulnerability", Catchphrase = null },
+                  new Superhero { Name = "Poison Ivy", Alterego = "Pamela Lillian Isley", PrimaryAbility = "Chlorokinesis", SecondaryAbility = "Toxic immunity", Catchphrase = null }
+                );
+                context.SaveChanges();
         }
     }
 }
