11e49ccc09a118e3067ab6c8b65df8a60192ef62
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-02 12:03:46 -0500

    adding program files

---
+++ SuperHero/Controllers/SuperHeroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHero.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuperHero/Models/Superhero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Alterego { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string Catchphrase { get; set; }
+
+    }
+}
