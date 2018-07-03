076810312f458ee8ffb96b51ecf2897d9c9aa18b
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-03 09:54:34 -0500

    completed controller actions

--- SuperHero/Controllers/SuperHeroController.cs
+++ SuperHero/Controllers/SuperHeroController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,17 +11,19 @@ namespace SuperHero.Controllers
     public class SuperHeroController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        // GET: SuperHero
+        
+        [HttpGet]
         public ActionResult Index()
         {
             return View(db.Superhero.ToList());
         }
 
-        public ActionResult Edit(int? id)
+        [HttpGet]
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
-                return HttpNotFound();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Superhero superhero = db.Superhero.Find(id);
             if (superhero == null)
@@ -28,6 +31,77 @@ namespace SuperHero.Controllers
                 return HttpNotFound();
             }
             return View(superhero);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create([Bind(Include = "ID,Name,Alterego,PrimaryAbility,SecondaryAbility,CatchPhrase")] Superhero superhero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Superhero.Add(superhero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superhero);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Superhero superhero = db.Superhero.Find(id);
+            if (superhero == null)
+            {
+                return HttpNotFound();
+            }
+            return View(superhero);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Alterego,PrimaryAbility,SecondaryAbility,CatchPhrase")] Superhero superhero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(superhero).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superhero);
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Superhero superhero = db.Superhero.Find(id);
+            if (superhero == null)
+            {
+                return HttpNotFound();
+            }
+            return View(superhero);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Superhero superhero = db.Superhero.Find(id);
+            db.Superhero.Remove(superhero);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
